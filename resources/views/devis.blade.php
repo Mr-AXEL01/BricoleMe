08203b35c21bcf144945c75f13686b40e3bcf028
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-02-23 22:45:24 +0100

    create section for informations for each client and artisan in the devis

--- resources/views/devis.blade.php
+++ resources/views/devis.blade.php
@@ -15,6 +15,16 @@
         <p>Phone: +212 565345589</p>
         <p>Email: BrecoleMe@services.com</p>
     </div>
+    <div class="section">
+        <h2 class="subtitle">For Mr:</h2>
+        <p><strong>Client Name:</strong> {{$client_name}}</p>
+    </div>
+    <div class="section">
+        <h2 class="subtitle">Artisan Information</h2>
+        <p><strong>Artisan Name:</strong> {{$artisan_name}}</p>
+        <p><strong>Artisan Phone:</strong> {{$artisan_phone}}</p>
+        <p><strong>Artisan Email:</strong> {{$artisan_email}}</p>
+    </div>
 
 </div>
 </body>
