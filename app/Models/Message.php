9ddda57ddd827d26723b29ba2cafbbc1b127a1bb
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-02-26 16:54:10 +0100

    create a controller for the chat

--- app/Models/Message.php
+++ app/Models/Message.php
@@ -8,4 +8,10 @@ use Illuminate\Database\Eloquent\Model;
 class Message extends Model
 {
     use HasFactory;
+
+    protected $fillable=[
+        'sender',
+        'receiver',
+        'message'
+    ];
 }
