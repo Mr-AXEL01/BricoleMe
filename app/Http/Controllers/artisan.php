78afa9bc5256843e7bcd7d886a2b58b4b88c43ad
sanaa-ennaji <sanaaennaji93@gmail.com>
2024-02-26 17:25:41 +0100

    commit

--- app/Http/Controllers/artisan.php
+++ app/Http/Controllers/artisan.php
@@ -6,34 +6,42 @@ use Illuminate\Http\Request;
 
 class artisan extends Controller
 {
-    public function register(Request $request)
-    {
-       
+   
     
-        public function create(Request $request)
-        {
-            $request->validate([
-                'images' => 'required|array',
-                'images.*' => 'image|mimes:jpeg,png,jpg,gif|max:2048', 
-                'description' => 'required|string',
-            ]);
+    public function storeOrUpdate(Request $request, $id = null)
+    {
+        $request->validate([
+            'images' => 'required|array',
+            'images.*' => 'image|mimes:jpeg,png,jpg,gif|max:2048', 
+            'description' => 'required|string',
+            'metier_id' => 'required|exists:jobs,id',
+            'competence_id' => 'required|exists:skills,id',
+        ]);
+
+        $uploadedImages = [];
+        foreach ($request->file('images') as $image) {
+            $path = $image->store('images'); 
+            $uploadedImages[] = $path;
+        }
+
+        $user_id = auth()->id();
+
+        if ($id) {
         
-            $uploadedImages = [];
-            foreach ($request->file('images') as $image) {
-                $path = $image->store('images'); 
-                $uploadedImages[] = $path;
-            }
-        
-            $user_id = auth()->id(); 
-        
-            $artisan = Artisan::create([
+            $artisan = Artisan::findOrFail($id);
+            $artisan->update([
                 'user_id' => $user_id,
                 'images' => $uploadedImages,
                 'description' => $request->description,
             ]);
-        
-            return redirect()->route('artisan.index')->with('success', 'Artisan created');
-        }
+
+       
+            $artisan->matiers()->sync([$request->metiers_id]);
+            $artisan->competences()->sync([$request->skill_id]);
+       
+
+        return redirect()->route('artisan')->with('success', 'Artisan ');
+    }
         
 
 }
