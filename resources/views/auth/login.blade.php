93e16e4f99949556a328a398ed2dd9e5ee133ed3
Mr-AXEL01 <abdelhak20azur02@gmail.com>
2024-02-28 15:28:08 +0100

    update the login to not have defrent background if we have deffrent mode

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -94,7 +94,7 @@
 
                 <div class="flex items-center justify-between mt-4">
                     @if (Route::has('password.request'))
-                        <a class="underline text-sm text-gray-600 dark:text-gray-400 hover:text-gray-900 dark:hover:text-gray-100 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500 dark:focus:ring-offset-gray-800"
+                        <a class="underline text-sm text-gray-600 hover:text-gray-900 rounded-md focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
                            href="{{ route('password.request') }}">
                             {{ __('Forgot your password?') }}
                         </a>
@@ -111,12 +111,8 @@
                 </div>
             </form>
 
-    </div>
+        </div>
 
 </section>
 </body>
 </html>
-
-
-
-
